68ce407ef5a17d44a1c9ed32a4fbf1b8ddd591ee
Matias Cancinos <smatiasnahuel@outlook.com>
2023-12-19 20:51:48 -0300

    Colores

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const button = document.querySelector('#check')
 const inputText = document.querySelector('#input-text')
 const resultado = document.querySelector('#resultado')
+const buttonRed = document.querySelector('.btn-red');
+const buttonGreen = document.querySelector('.btn-green');
+const buttonBlue = document.querySelector('.btn-blue');
+const body = document.querySelector('body');
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
     inputText.focus(); 
   });
 button.addEventListener('click', inputFunction)
+
 
 function inputFunction(){
     const texto = inputText.value.trim()
@@ -28,5 +35,21 @@ function inputFunction(){
         resultado.classList.add(esPalindromo ? 'exito' : 'error')
     }, 10);
 }
+
+buttonRed.addEventListener('click', () => {
+    body.style.background = 'linear-gradient(to right, #FF1F22, #E00004)'
+    button.style.background = '#FF1F22'
+})
+
+buttonGreen.addEventListener('click', function() {
+    body.style.background = 'linear-gradient(to right, #00cc00, #08D93C)';
+    button.style.background = '#00cc00'
+});
+
+buttonBlue.addEventListener('click', function() {
+    body.style.background = 'linear-gradient(to right, #0F00B8, #0E00A3)';
+    button.style.background = '#0F00B8'
+
+});
 
 
